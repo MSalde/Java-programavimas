5fff4b6cf091f5493fb296d0088d9d4126642b03
MSalde <saldys.mindaugas@gmail.com>
2023-05-04 11:45:47 +0300

    Pridetos 11 (JS) paskaitos uzduotys.

--- Paskaita 11. Java script/Užduotis 3.js
+++ Paskaita 11. Java script/Užduotis 3.js
@@ -1,23 +1,18 @@
-window.onload = function(){
-let = trigubasuma;
-console.log(trigubasuma);
-let rezultatas = trigubasuma(6, 4);
-console.log(rezultatas);
-function trigubasuma(a, b){
-    return 3 * (a + b);
+window.onload = function () {
+
+    let rezultatas = trigubaSuma(6, 4);
+    console.log(rezultatas);
+
+    let rezultatas1 = trigubaSuma(3, 5);
+    console.log(rezultatas1);
+
+    let rezultatas2 = trigubaSuma(-2, 11);
+    console.log(rezultatas2);
+
+    let rezultatas3 = trigubaSuma(50, 20);
+    console.log(rezultatas3);
+
 }
-let a = 3;
-let b = 5;
-let rezultatas2 = trigubasuma (a, b);
-console.log(rezultatas2);
-
-let x = -2;
-let y = 11;
-let rezultatas3 = trigubasuma (x, y);
-console.log(rezultatas3);
-
-let sk1 = 50;
-let sk2 = 20;
-let rezultatas4 = trigubasuma (sk1, sk2);
-console.log(rezultatas4);
+function trigubaSuma(a, b) {
+    return 3 * (a + b);
 }

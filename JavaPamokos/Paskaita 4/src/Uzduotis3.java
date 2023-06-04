cbf25c1ad7fa4ab7f9946bd9f7fc6c682bc3c6bb
MSalde <saldys.mindaugas@gmail.com>
2023-06-04 12:58:17 +0300

    Paskaita 25.

--- JavaPamokos/Paskaita 4/src/Uzduotis3.java
+++ JavaPamokos/Paskaita 4/src/Uzduotis3.java
@@ -11,22 +11,29 @@ public class Uzduotis3 {
         Scanner skait3 = new Scanner(System.in);
         System.out.println("Įveskite trikampio krastines c ilgi");
         double c = skait1.nextDouble();
-        System.out.println("Trikampio plotas yra: " + trikampioPlotas(a, b, c));
+        System.out.println("Trikampis yra: " + trikampioTipas(a, b, c));
+        double p2 = ((a + b + c) /2);
+        System.out.println("Trikampio plotas yra: " + trikampioPlotas(p2, a, b, c));
         System.out.println("Trikampio perimetras yra: " + trikampioPerimetras(a, b, c));
+
     }
 
-    public static String trikampioPlotas(double a, double b, double c) {
+    public static String trikampioTipas(double a, double b, double c) {
+
         if (c * c == (a * a) + (b * b)) {
-            return "Trikampis yra statusis";
+            return "Statusis";
         }
         else if (a == b && b == c && a == c) {
-            return "Trikampis yra lygiakrastis";
+            return "Lygiakrastis";
         }
         else if (a == b || b == c || a == c) {
-            return "Trikampis yra lygiasonis";
+            return "Lygiasonis";
         }else{
-            return "Trikampis yra paprastas";
+            return "Paprastas";
         }
+    }
+    public static double trikampioPlotas (double p2, double a, double b, double c) {
+        return Math.sqrt (p2 * (p2 - a) * (p2 - b) * (p2 - c));
     }
 
     public static double trikampioPerimetras (double a, double b, double c) {

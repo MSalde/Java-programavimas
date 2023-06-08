7211fe6341f5efd347d6a6a8f00493b02c303df0
MSalde <saldys.mindaugas@gmail.com>
2023-06-08 09:22:37 +0300

    Paskaita 28.

--- JavaPamokos/Paskaita 6/src/Uzduotis2.java
+++ JavaPamokos/Paskaita 6/src/Uzduotis2.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 public class Uzduotis2 {
     public static void main(String[] args) {
+        isvestiSkaicius(8, 5);
+    }
 
+    public static void isvestiSkaicius(int pradzia, int pabaiga) {
+        for (int i = pradzia; i >= pabaiga; i--) {
+            System.out.println(i);
+        }
     }
 }

c641a024478208f59db369b4f2321d32449360bd
MSalde <saldys.mindaugas@gmail.com>
2023-06-08 09:14:30 +0300

    Paskaita 28.

---
+++ JavaPamokos/Paskaita 6/src/Uzduotis1.java
@@ -0,0 +1,13 @@
+public class Uzduotis1 {
+    public static void main(String[] args) {
+
+        isvestiSkaicius(1, 10);
+
+    }
+
+    public static void isvestiSkaicius(int pradzia, int pabaiga) {
+        for (int i = pradzia; i <= pabaiga; i++) {
+            System.out.println(i);
+        }
+    }
+}

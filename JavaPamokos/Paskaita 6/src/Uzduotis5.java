4d860704d7e61cc70f5c3500cfaafaffea2920c8
MSalde <saldys.mindaugas@gmail.com>
2023-06-08 20:00:18 +0300

    Paskaita 28.

--- JavaPamokos/Paskaita 6/src/Uzduotis5.java
+++ JavaPamokos/Paskaita 6/src/Uzduotis5.java
@@ -1,9 +1,9 @@
 public class Uzduotis5 {
     public static void main(String[] args) {
         int skaicius = 15;
-        System.out.println(arDalinasiBeLiekanos(skaicius));
+        System.out.println(arSkaiciusPirminis(skaicius));
     }
-        public static boolean arDalinasiBeLiekanos (int skaicius) {
+        public static boolean arSkaiciusPirminis (int skaicius) {
             int kiekis = 0;
             for (int i = 2; i < skaicius; i++) {
                 if (skaicius % i == 0) {
